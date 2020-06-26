266bf35028fac3289b054bd8b340aea3f57843cb
dhokim <dhokim@smtown.com>
2020-06-26 14:26:36 +0900

    make db before Default Setting

--- src/main/java/com/spring/basic/controller/MainController.java
+++ src/main/java/com/spring/basic/controller/MainController.java
@@ -1,9 +1,7 @@
 package com.spring.basic.controller;
 
-import com.spring.basic.model.user.User;
-import com.spring.basic.repository.UserMapper;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Created by dhokim on 2020-06-24
@@ -12,28 +10,10 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class MainController {
 
-    private UserMapper userMapper;
-
     @GetMapping("/")
     public String home(){
         System.out.println("Connect Main Controller : /");
         return "/WEB-INF/main.jsp";
     }
 
-    @PostMapping("/user/add")
-    @ResponseBody
-    public String addNewUser(@RequestParam String name, @RequestParam String email){
-        User user = new User();
-        user.setName(name);
-        user.setEmail(email);
-        userMapper.save(user);
-
-        return "Saved";
-    }
-
-    @PostMapping("/all")
-    @ResponseBody
-    public Iterable<User> getAllUser(){
-        return userMapper.findAll();
-    }
 }
